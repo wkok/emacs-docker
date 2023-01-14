3c6c67c17a42bbd3a42ece16380deaa01897c94d
Werner Kok <wernerkok@gmail.com>
2023-01-14 10:45:59 +0000

    bump dependencies

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --tag wkok/dev:2022-10-23 .
+docker build --tag wkok/dev:2023-01-14 .
--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-git clone git@github.com:wkok/Emacs-configuration.git $HOME/src/emacs-config
+git clone git@github.com:wkok/emacs-config.git $HOME/src/emacs-config
 
-# Setup ssh first: https://docs.github.com/en/authentication/connecting-to-github-with-ssh
-cd $HOME/src/emacs-config
-git submodule update --init
-
-echo '(setq base "~/src/emacs-config/")' > $HOME/.emacs && \
-echo '(load (concat base "ca-init.el"))' >> $HOME/.emacs
+cp $HOME/src/emacs-config/dot-emacs $HOME/.emacs
